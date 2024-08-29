3d014bffb7ee43be84389f88f583d781bae8f68d
Jpool <jhanpaulemeza@gmail.com>
2024-08-28 20:40:40 -0500

    Actualiazionces CSS

--- static/Js/Index1.js
+++ static/Js/Index1.js
@@ -1,47 +1,62 @@
 document.addEventListener("DOMContentLoaded", function() {
     var sidebar = document.getElementById("sidebar");
     var toggleButton = document.getElementById("toggle-button");
+    var loaderOverlay = document.getElementById("loader-overlay");
+    var contentSection = document.querySelector(".content");
 
-    // Verifica el estado guardado en localStorage
-    var sidebarState = localStorage.getItem('sidebarState');
-    if (sidebarState === 'open') {
-        sidebar.style.left = '0';
-        toggleButton.innerHTML = 'X';
-        toggleButton.style.marginLeft = '234px';
-        toggleButton.classList.add('open');
-    } else {
-        sidebar.style.left = '-250px';
-        toggleButton.innerHTML = '☰';
-        toggleButton.style.marginLeft = '4px';
-        toggleButton.classList.remove('open');
-    }
+    // var sidebarState = localStorage.getItem('sidebarState');
+    // if (sidebarState === 'open') {
+    //     sidebar.style.left = '0';
+    //     toggleButton.innerHTML = 'X';
+    //     toggleButton.style.marginLeft = '234px';
+    //     toggleButton.classList.add('open');
+    // } else {
+    //     sidebar.style.left = '-250px';
+    //     toggleButton.innerHTML = '☰';
+    //     toggleButton.style.marginLeft = '4px';
+    //     toggleButton.classList.remove('open');
+    // }
 
-    // Añade el evento click para alternar la barra lateral
     toggleButton.addEventListener("click", function() {
         if (sidebar.style.left === "-230px" || sidebar.style.left === "") {
             sidebar.style.left = "0";
             toggleButton.innerHTML = "X";
             toggleButton.style.marginLeft = "234px";
             toggleButton.classList.add('open');
-            localStorage.setItem('sidebarState', 'open'); // Guarda el estado en localStorage
+            kcontentSection.classList.add('sidebar-open'); // Añadir la clase cuando se abre el sidebar
+            localStorage.setItem('sidebarState', 'open'); 
         } else {
             sidebar.style.left = "-230px";
             toggleButton.innerHTML = "☰";
             toggleButton.style.marginLeft = "4px";
             toggleButton.classList.remove('open');
-            localStorage.setItem('sidebarState', 'closed'); // Guarda el estado en localStorage
+            // contentSection.classList.remove('sidebar-open'); // Remover la clase cuando se cierra el sidebar
+            localStorage.setItem('sidebarState', 'closed');
         }
     });
+
+    document.getElementById('toggle-button').addEventListener('click', function() {
+        document.getElementById('sidebar').classList.toggle('collapsed');
+        document.querySelector('.content').classList.toggle('expanded');
+    });
+    document.querySelectorAll('.loader-trigger').forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            loaderOverlay.style.display = 'flex';
+            
+            setTimeout(function() {
+                window.location.href = link.href; 
+            }, 500);
+        });
+    });
+    document.getElementById('logoutButton').addEventListener('click', function() {
+        window.location.href = '/logout';
+    });
 });
-
-
-
 
 document.querySelectorAll('.sidebar ul > li > a').forEach(function(item) {
     item.addEventListener("click", function(e) {
         var sublist = this.nextElementSibling;
-
-        // Solo prevenir el comportamiento predeterminado si hay una sublista
         if (sublist && sublist.classList.contains('sublist')) {
             e.preventDefault();
 
